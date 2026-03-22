a74aa8bb9cad37ae735ce9dffdeb2c224ed421c0
mcaforo <peter.aforo@mcaforo.com>
2026-03-22 17:24:45 +0000

    fix: Replace deprecated apple-mobile-web-app-capable meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,7 +76,7 @@ export default async function RootLayout({
         {/* PWA */}
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#3D4DB7" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="TMHT Presby" />
         <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
