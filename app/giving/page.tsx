9706a3ca31e81e8a66d3da2e477117ebdb55bdba
mcaforo <peter.aforo@mcaforo.com>
2026-03-23 22:50:43 +0000

    fix: Remove unused Link import from giving page

--- app/giving/page.tsx
+++ app/giving/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import Link from "next/link";
 import { PageHeroWithBackground } from "@/components/layout/PageHeroWithBackground";
 import {
   Heart,
